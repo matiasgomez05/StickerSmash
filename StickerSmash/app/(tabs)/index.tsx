8cf879bde45a179b0016b220e9a34d5d14a6bfd4
Matias Gomez <matias_gomez05@hotmail.com>
2025-04-03 16:38:04 -0300

    Modificaciones generales; botones y diseño

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { View, StyleSheet } from 'react-native';
+import ImageViewer from '../components/imageViewer';
+import Button from '../components/button';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
-    <View style = { styles.container }>
-      <Text style = { styles.text }> Inicio </Text>
-      <Link href="/productos" style={styles.button}>
-        Ir a Productos
-      </Link>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={ PlaceholderImage } />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -17,14 +23,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
+    alignItems: 'center'
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
   },
 });
